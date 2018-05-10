026ca10ab71abaf13116335889b4e400bfcd1f1e
mefryar <mfryar.hks@gmail.com>
2018-05-09 21:38:15 -0400

    Add Global Gitignore

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,8 +8,8 @@
 ## Dotfiles directory
 dir=~/dotfiles
 ## Files to symlink in homedir
-files="bash_profile bashrc gitconfig git-completion.bash git-prompt.sh"
-
+files="bash_profile bashrc gitconfig git-completion.bash gitignore
+git-prompt.sh"
 # Create symlinks from the homedir to files in the ~/dotfiles directory
 for file in $files; do
     echo "Creating symlink to $file in home directory."
